24ba79327a4094470a8942be1047c33dc5d64cd5
Bhardwaj Thummar <50964723+BhardwajThummar@users.noreply.github.com>
2022-05-11 18:42:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const employeeController = require("./controllers/employeeController");
-const home = require("./controllers/home");
-const path = require("path");
-const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-const {engine} = require("express-handlebars");
-const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
+// const express = require("express");
+// const employeeController = require("./controllers/employeeController");
+// const home = require("./controllers/home");
+// const path = require("path");
+// const exphbs = require("express-handlebars");
+// const bodyParser = require("body-parser");
+// const {engine} = require("express-handlebars");
+// const mongoose = require("mongoose");
+// const port = process.env.PORT || 5000;
 
-var app = express();
+// var app = express();
 
-//DB connection
-mongoose.connect(   
-    "mongodb+srv://BHARDWAJ:YhnYmJXjGnvXgxAN@cluster0.voeu8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-    ()=> console.log('Connected to DB is not connected to DBBDBDBSHFBSDBFDL')
-);
+// //DB connection
+// mongoose.connect(   
+//     "mongodb+srv://BHARDWAJ:YhnYmJXjGnvXgxAN@cluster0.voeu8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+//     ()=> console.log('Connected to DB is not connected to DBBDBDBSHFBSDBFDL')
+// );
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(bodyParser.json());
 
-//express-handlebars
-app.engine("handlebars", engine({ defaultLayout: "main" }));
-app.engine('hbs', engine({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
-app.set('view engine', 'hbs');
+// //express-handlebars
+// app.engine("handlebars", engine({ defaultLayout: "main" }));
+// app.engine('hbs', engine({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+// app.set('view engine', 'hbs');
 
-app.listen(port,() => {
-    console.log(`Server started at port ${port}`);
-});
+// app.listen(port,() => {
+//     console.log(`Server started at port ${port}`);
+// });
 
-app.use('/', home);
+// app.use('/', home);
 
-app.use('/employee', employeeController);
+// app.use('/employee', employeeController);
